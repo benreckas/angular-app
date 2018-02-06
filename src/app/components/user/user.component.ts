05463cb4251068d8074538258d5d9aae0f78a93f
benreckas <benreckas@gmail.com>
2018-02-05 17:34:29 -0800

    Add Models folder.

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from '../../models/User';
 
 @Component({
   selector: 'app-user',
@@ -8,42 +9,19 @@ import { Component } from '@angular/core';
 
 export class UserComponent {
   // Properties
-  firstName: string;
-  lastName: string;
-  age: number;
-  address;
-
-
-  foo: any;
-  hasKids: boolean;
-  numberArr: number[];
-  stringArr: string[];
-  mixedArr: any[];
-  myTuple: [string, number, boolean];
+  user: User;
 
   // Methods
   constructor() {
-    this.firstName = 'Ben';
-    this.lastName = 'Reckas';
-    this.age = 28;
-    this.address = {
-      street: '50 Main Street',
-      city: 'Clovis',
-      state: 'CA' 
-    };
-    this.numberArr = [1, 2, 3];
-    this.stringArr = ['banana', 'apple'];
-    this.mixedArr = ['banana', 'apple', 1, 2, true, undefined];
-    this.myTuple = ['ben', 28, false];
-
-    console.log(this.addNums(2, 3));
-  }
-
-  showAge() {
-    return this.age + 2;
-  }
-
-  addNums(num1: number , num2: number): number {
-    return num1 + num2;
+    this.user = {
+      firstName: 'Ben',
+      lastName: 'Reckas',
+      age: 28,
+      address: {
+        street: '50 Main Street',
+        city: 'Clovis',
+        state: 'CA'
+      }
+    }
   }
 }
